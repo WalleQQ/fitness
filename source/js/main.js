edbe242b40da06910b10a9da1c67a8a78796db85
Vitalii Dragulin <80743821+WalleQQ@users.noreply.github.com>
2022-05-12 18:59:49 +0400

    hпавки после код ревью

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,5 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {coachesSwiper, reviewsSwiper} from '../js/modules/swiper.js';
+import '../js/modules/swiper.js';
 import '../js/modules/video.js';
 import '../js/modules/tabs.js';
 import '../js/modules/validation.js';
@@ -8,7 +8,5 @@ import '../js/modules/validation.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   iosVhFix();
-  coachesSwiper();
-  reviewsSwiper();
   window.addEventListener('load', () => {});
 });
--- source/js/modules/utils.js
+++ source/js/modules/utils.js
@@ -1,1 +1,2 @@
 export const isEscapeKey = (evt) => evt.key === 'Escape';
+export const isEnterKey = (evt) => evt.key === 'Enter';
